45567bc72f61414f9a18564d70f56a9de9b6ac12
Mehmet OKUYAR <58508173+MehmetOKUYAR@users.noreply.github.com>
2021-08-21 12:57:20 +0300

    auto_video_labeling

--- auto_label_codes.py
+++ auto_label_codes.py
@@ -1,11 +1,12 @@
 from auto_label_python import Ui_MainWindow
-from PyQt5.QtWidgets import QMainWindow, QFileDialog
+from PyQt5.QtWidgets import QMainWindow, QFileDialog,QMessageBox
 from PyQt5.QtGui import QIntValidator
 
 import cv2
 import os
 from pathlib import Path
 import numpy as np
+import sys
 
 
 class auto_label(QMainWindow):
@@ -20,7 +21,7 @@ class auto_label(QMainWindow):
         self.ui.lineEdit_classId.setValidator(QIntValidator(0,100000000,self))
         self.ui.pushButton_loadVideo.clicked.connect(self.load_video)
         self.ui.pushButton_start.clicked.connect(self.start)
-        
+        self.fileName = 0
 
     def load_video(self):
         self.fileName = QFileDialog.getOpenFileName()[0]
@@ -29,8 +30,11 @@ class auto_label(QMainWindow):
     def start(self):
 
         initBB = None
-
+        
+     
         vs = cv2.VideoCapture(self.fileName)
+
+
         
         if self.ui.lineEdit_classname.text() == '':
             class_name = 'label'
@@ -97,69 +101,76 @@ class auto_label(QMainWindow):
         saved_image = 0
         new_H = 1520
         new_W = 880
-        while True:
-            ret,frame = vs.read()
-            if ret is None:
-                break
-            frame_count +=1
-            image = frame.copy()
+        if not vs.isOpened():
+            QMessageBox.warning(self,'Warning Message',' The video could not be opened. Please check the file type ! ')
+            self.load_video()
             
-            frame = cv2.resize(frame,(new_H,new_W))
-            (H, W) = frame.shape[:2]
-            
-            
-            if initBB is not None:
+        else:    
+            while True:
+                ret,frame = vs.read()
                 
-                (success, box) = tracker.update(frame)
-                # check to see if the tracking was a success
-                if success:
-                    # for box in boxes:
-                    (x, y, w, h) = [int(v) for v in box]
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                if ret is None:
+                    sys.exit()
+                    break
+                frame_count +=1
+                image = frame.copy()
+                
+                frame = cv2.resize(frame,(new_H,new_W))
+                (H, W) = frame.shape[:2]
+                
+                
+                if initBB is not None:
                     
-                    if frame_count%per_frame == 0:
-                        count += 1
-                        saved_image +=1
-                        save_img_txt(image, box, count,H,W,class_id)
+                    (success, box) = tracker.update(frame)
+                    # check to see if the tracking was a success
+                    if success:
+                        # for box in boxes:
+                        (x, y, w, h) = [int(v) for v in box]
+                        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                        
+                        if frame_count%per_frame == 0:
+                            count += 1
+                            saved_image +=1
+                            save_img_txt(image, box, count,H,W,class_id)
 
-                info = [
-                    ("Success", "Yes" if success else "No"),
-                    ("Saved images: ",f'{saved_image}')
-                ]
-        
-                for (i, (k, v)) in enumerate(info):
-                    text = "{}: {}".format(k, v)
-                    cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+                    info = [
+                        ("Success", "Yes" if success else "No"),
+                        ("Saved images: ",f'{saved_image}')
+                    ]
+            
+                    for (i, (k, v)) in enumerate(info):
+                        text = "{}: {}".format(k, v)
+                        cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+                                    cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
 
-            cv2.imshow("Frame", frame)
-            key = cv2.waitKey(1) & 0xFF
-            # if the 's' key is selected, we are going to "select" a bounding
-            # box to track
-            if key == ord("s"):
-                initBB = cv2.selectROI(
-                    "Frame", frame, fromCenter=False, showCrosshair=True)
-                try:
-                    tracker = cv2.TrackerCSRT_create()
-                    tracker.init(frame, initBB)
-                except:
-                    pass
+                cv2.imshow("Frame", frame)
+                key = cv2.waitKey(1) & 0xFF
+                # if the 's' key is selected, we are going to "select" a bounding
+                # box to track
+                if key == ord("s"):
+                    initBB = cv2.selectROI(
+                        "Frame", frame, fromCenter=False, showCrosshair=True)
+                    try:
+                        tracker = cv2.TrackerCSRT_create()
+                        tracker.init(frame, initBB)
+                    except:
+                        pass
 
-            elif key == ord("a") :
-                new_H += 50
-            elif key == ord("z") :
-                new_H -= 50
+                elif key == ord("a") :
+                    new_H += 50
+                elif key == ord("z") :
+                    new_H -= 50
 
 
-            elif key == ord("w") :
-                new_W += 50
-            elif key == ord("e") :
-                new_W -= 50
+                elif key == ord("w") :
+                    new_W += 50
+                elif key == ord("e") :
+                    new_W -= 50
 
-                
-                # if the `q` key was pressed, break from the loop
-            elif key == ord("q"):
-                break
+                    
+                    # if the `q` key was pressed, break from the loop
+                elif key == ord("q"):
+                    break
 
        
         vs.release()
